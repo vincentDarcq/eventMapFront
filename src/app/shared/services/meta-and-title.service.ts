90c94e81ae8f6b01b82bfe68ef7b1cf33c81e38f
Vincent Darcq <vincentdarcq@MacBook-Air-de-Vincent.local>
2022-10-15 18:46:54 +0200

    fixes

--- src/app/shared/services/meta-and-title.service.ts
+++ src/app/shared/services/meta-and-title.service.ts
@@ -28,19 +28,18 @@ export class MetaAndTitleService implements OnDestroy {
   }
 
   private updateTitle(url: string): void {
-    if(typeof metaData[this.urlPaths[1]] === "undefined"){
-      this.title.setTitle(metaData['/'].title);
-    }else{
-      this.title.setTitle(metaData[this.urlPaths[1]].title);
-    }
+    const index = "/"+this.urlPaths[1];
+    this.title.setTitle(metaData[index].title);
   }
 
   private updateMeta(url: string): void {
     const oldTagDescription = this.meta.getTag('name="description"');
+    const index = "/"+this.urlPaths[1];
+    console.log(index)
 
     const newTagDescription = {
       name: 'description',
-      content: metaData["/"+this.urlPaths[1]].metas.description
+      content: metaData[index].metas.description
     }
 
     oldTagDescription
